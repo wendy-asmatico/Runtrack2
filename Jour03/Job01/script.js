f015a71aa0078871d61507c5735ddbb36f69a7d0
wendy-asmatico <wendy.asmatico@laplateforme.io>
2025-11-19 16:07:01 +0100

    job1 finit

---
+++ Jour03/Job01/script.js
@@ -0,0 +1,19 @@
+
+const texte = document.getElementById("texte");
+const button = document.getElementById("button");
+const fermer = document.getElementById("fermer");
+
+
+button.addEventListener("click", (event) => {
+
+    texte.style.visibility = "visible";
+    fermer.style.visibility = "visible";
+    button.style.visibility = "hidden";
+});
+
+fermer.addEventListener("click", (event) => {
+
+    texte.style.visibility = "hidden";
+    fermer.style.visibility = "hidden";
+    button.style.visibility = "visible";
+});
